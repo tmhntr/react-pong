4c6232dfb3d1722b7dbee9db17d01972b8bc7882
Tim Hunter <thunte27@uwo.ca>
2022-03-23 01:30:39 -0400

    Reverted Pong to class component with props

--- src/components/Pong/Pong.tsx
+++ src/components/Pong/Pong.tsx
@@ -4,19 +4,26 @@ import * as pongGame from "../../utils/pongGame.js";
 import * as pg from "../../utils/pyjsdl.js";
 import { WebPaddleAgent } from "../../utils/WebPaddleAgent.js";
 
+export type PongProps = { wsAddress?: string };
 
-const Pong = ({wsAddress}) => {
-  // var game: any, agent: any
-  useEffect(() => {
+export default class Pong extends Component<PongProps>{
+  // constructor(props) {
+  //   super(props);
+  //   // Don't call this.setState() here!
+  //   this.state = { counter: 0 };
+  // }
+  componentDidMount () {
     var game = pongGame.Game();
     game.PLAY_SOUND = false;
-    var agent = WebPaddleAgent(pongGame.LEFT, game, wsAddress);
-    game.set_left_paddle(agent);
+    if (this.props.wsAddress) {
+      var agent = WebPaddleAgent(pongGame.LEFT, game, this.props.wsAddress);
+      game.set_left_paddle(agent);
+    } 
     pg.setup(game);
-  }, [])
+  }
+  render () {
 
-  return <div id="__panel__"></div>;
-  
+    return <div id="__panel__"></div>;
+  } 
 };
 
-export default Pong
