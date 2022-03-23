af935b9690772ff93880631be68628a0dee7e0ac
Tim Hunter <thunte27@uwo.ca>
2022-03-23 00:57:33 -0400

    Add imports into Pong.tsx

--- src/components/Pong/Pong.tsx
+++ src/components/Pong/Pong.tsx
@@ -1,19 +1,21 @@
-import React, { useEffect } from "react";
+import React, { useEffect, Component } from "react";
 
 import * as pongGame from "../../utils/pongGame.js";
 import * as pg from "../../utils/pyjsdl.js";
 import { WebPaddleAgent } from "../../utils/WebPaddleAgent.js";
 
 
-const Pong = () => {
-  const game = pongGame.Game();
-  game.PLAY_SOUND = false;
-  useEffect(() => {
-    const agent = WebPaddleAgent(pongGame.LEFT, game);
-    game.set_left_paddle(agent);
-    pg.setup(game);
-  }, []);
-  return <div id="__panel__"></div>;
+export default class Pong extends Component{
+  componentDidMount () {
+    // var game = pongGame.Game();
+    // game.PLAY_SOUND = false;
+    // var agent = WebPaddleAgent(pongGame.LEFT, game);
+    // game.set_left_paddle(agent);
+    // pg.setup(game);
+  }
+  render () {
+
+    return <div id="__panel__"></div>;
+  } 
 };
 
-export default Pong
