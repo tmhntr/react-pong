43253bbeb4b26ad7c8294df78e385dafb3b48f02
Tim Hunter <thunte27@uwo.ca>
2022-03-23 02:07:18 -0400

    Dev

--- src/components/Pong/Pong.tsx
+++ src/components/Pong/Pong.tsx
@@ -4,19 +4,17 @@ import * as pongGame from "../../utils/pongGame.js";
 import * as pg from "../../utils/pyjsdl.js";
 import { WebPaddleAgent } from "../../utils/WebPaddleAgent.js";
 
-export type PongProps = { wsAddress?: string };
+export type PongProps = { wsAddress: string };
 
 export default class Pong extends Component<PongProps>{
-  // constructor(props) {
-  //   super(props);
-  //   // Don't call this.setState() here!
-  //   this.state = { counter: 0 };
-  // }
   componentDidMount () {
+    console.log(this.props)
     var game = pongGame.Game();
     game.PLAY_SOUND = false;
-    if (this.props.wsAddress) {
-      var agent = WebPaddleAgent(pongGame.LEFT, game, this.props.wsAddress);
+    const { wsAddress} = this.props
+    if (wsAddress) {
+      console.log(wsAddress)
+      var agent = WebPaddleAgent(pongGame.LEFT, game, wsAddress);
       game.set_left_paddle(agent);
     } 
     pg.setup(game);
